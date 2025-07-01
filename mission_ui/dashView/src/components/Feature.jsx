2ee027ee943651142ee26bf6efd05168f7c6bc7e
Ashutosh3524 <ashuzx03@gmail.com>
2025-07-02 02:59:47 +0530

    home fixed navbar fixed sigup added temp signin,feature,contact page added

--- mission_ui/dashView/src/components/Feature.jsx
+++ mission_ui/dashView/src/components/Feature.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Feature = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-3xl font-bold text-white-800">Feature Page Reached ✅</h1>
+      <h1 className="text-3xl font-bold text-white-800" style = {{color: 'whitesmoke'}} >Feature Page Reached ✅</h1>
     </div>
   );
 };
