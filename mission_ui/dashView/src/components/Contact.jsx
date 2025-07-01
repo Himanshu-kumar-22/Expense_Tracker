d94cc351416a8712402a94c715b0c27c8ca6c982
Ashutosh3524 <ashuzx03@gmail.com>
2025-07-02 02:59:47 +0530

    home fixed navbar fixed sigup added temp signin,feature,contact page added

--- mission_ui/dashView/src/components/Contact.jsx
+++ mission_ui/dashView/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-3xl font-bold text-white-800">Contact Page Reached ✅</h1>
+      <h1 className="text-3xl font-bold text-white-800" style = {{color: 'whitesmoke'}}>Contact Page Reached ✅</h1>
     </div>
   );
 };
